516eb0a9a9eeb0c82ef1117bce949404071ccf0d
JjungminLee <julie0964@naver.com>
2023-07-31 17:00:41 +0900

    fix:디자인 토큰 경로 변경

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,12 +1,10 @@
-import { COLORS } from "../../assets/styles/constants/colors";
-import { TEXT_STYLES } from "../../assets/styles/constants/textStyles";
+import { COLORS } from "../../../styles/constants/colors";
 import styled from "@emotion/styled";
-import Home from "../../assets/styles/icons/Home/Home_off.svg";
-
+import ALARM from "../../assets/gifImage/alarm2.gif";
 const TestPage = () => (
   <>
     <Text>서비스를 사용하는 동안 항상 허용</Text>
-    <img src={Home}></img>
+    <img src={ALARM} width={500} height={500}></img>
     <Text2>키즈 가디언이 아이들에게 알림을 줄 수 있도록 허용해주세요</Text2>
   </>
 );
@@ -15,9 +13,6 @@ export default TestPage;
 
 const Text = styled.div`
   color: ${COLORS.main.primary};
-  ${TEXT_STYLES.TEST}
 `;
 
-const Text2 = styled.div`
-  ${TEXT_STYLES.TEST2}
-`;
+const Text2 = styled.div``;

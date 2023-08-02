908bfd1c202bd72262472cae44b67593b21f3a66
ballsona0925@naver.com <ballsona0925@naver.com>
2023-08-02 12:26:05 +0900

    feat: install vite plugins for html-env and svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import VitePluginSvgr from 'vite-plugin-svgr';
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginSvgr(), VitePluginHtmlEnv({ compiler: true })],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },

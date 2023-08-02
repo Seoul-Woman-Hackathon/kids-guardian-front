d34f6f279f6042b36aff25d4bd1c318ff15e16d7
ballsona0925@naver.com <ballsona0925@naver.com>
2023-08-02 12:27:59 +0900

    feat: update useGeolocation

--- src/utils/useGeolocation.ts
+++ src/utils/useGeolocation.ts
@@ -1,7 +1,16 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 
+const options = {
+  /** 위치 정확도 (true면 더 정확한 위치를 제공하나 응답 속도 저하 및 전력 소모량 증가)*/
+  enableHighAccuracy: false,
+  /** 캐시에 지정한 위치 정보를 반환할 수 있는 최대 시간 */
+  maximumAge: 0,
+  /** 위치를 반환할 때 소모할 수 있는 최대 시간  */
+  timeout: 1000,
+};
+
 interface GeolocationInfoType {
-  loading: boolean;
+  isLoaded: boolean;
   error: GeolocationPositionError | null;
   timestamp: number | null;
   latitude: number | null;
@@ -9,18 +18,10 @@ interface GeolocationInfoType {
 }
 
 const useGeolocation = () => {
-  const watchIdRef = useRef<number>(0);
-  const optionsRef = useRef<PositionOptions>({
-    /** 위치 정확도 (true면 더 정확한 위치를 제공하나 응답 속도 저하 및 전력 소모량 증가)*/
-    enableHighAccuracy: true,
-    /** 캐시에 지정한 위치 정보를 반환할 수 있는 최대 시간 */
-    maximumAge: 0,
-    /** 위치를 반환할 때 소모할 수 있는 최대 시간  */
-    timeout: 5000,
-  });
+  const intervalId = useRef<number>(0);
 
   const [geolocationInfo, setGelocationInfo] = useState<GeolocationInfoType>({
-    loading: true,
+    isLoaded: false,
     error: null,
     timestamp: null,
     latitude: null,
@@ -28,45 +29,44 @@ const useGeolocation = () => {
   });
 
   // 현재 좌표 추출에 성공했을 경우
-  const onEventSuccess = ({ coords, timestamp }: GeolocationPosition) => {
-    const { latitude, longitude } = coords;
+  const onEventSuccess = useCallback(
+    ({ coords, timestamp }: GeolocationPosition) => {
+      const { latitude, longitude } = coords;
 
-    setGelocationInfo((prev) => ({
-      ...prev,
-      loading: false,
-      latitude,
-      longitude,
-      timestamp,
-    }));
-  };
+      setGelocationInfo((prev) => ({
+        ...prev,
+        isLoaded: true,
+        latitude,
+        longitude,
+        timestamp,
+      }));
+    },
+    [],
+  );
 
   // 현재 좌표 추출에 실패했을 경우
   const onEventError = useCallback((error: GeolocationPositionError) => {
     setGelocationInfo((prev) => ({
       ...prev,
-      loading: false,
       error,
     }));
-    alert(error.message);
+    console.error(error.message);
   }, []);
 
   useEffect(() => {
-    if (navigator.geolocation) {
+    if (!navigator.geolocation) return;
+
+    // 2초마다 한번씩 사용자 위치 업데이트
+    intervalId.current = setInterval(() => {
       navigator.geolocation.getCurrentPosition(
         onEventSuccess,
         onEventError,
-        optionsRef.current,
+        options,
       );
+    }, 2000);
 
-      watchIdRef.current = navigator.geolocation.watchPosition(
-        onEventSuccess,
-        onEventError,
-        optionsRef.current,
-      );
-    }
-
-    return navigator.geolocation.clearWatch(watchIdRef.current);
-  }, [optionsRef.current]);
+    return () => clearInterval(intervalId.current);
+  }, []);
 
   return geolocationInfo;
 };

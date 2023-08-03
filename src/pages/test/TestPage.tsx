a546e130320924b3b87a32a6a436706899080a87
ballsona0925@naver.com <ballsona0925@naver.com>
2023-08-03 21:55:29 +0900

    fix: delete unused components in TestPage

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,18 +1,5 @@
-import COLORS from '@styles/constants/colors';
-
-import styled from '@emotion/styled';
-import ALARM from '../../assets/gifImage/alarm2.gif';
 import AlarmFinishContent from '@/components/AlarmFinishContent';
-const TestPage = () => (
-  <>
-    <AlarmFinishContent />
-  </>
-);
+
+const TestPage = () => <AlarmFinishContent />;
 
 export default TestPage;
-
-const Text = styled.div`
-  color: ${COLORS.main.primary};
-`;
-
-const Text2 = styled.div``;

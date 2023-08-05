8ecc4aa774c4aa93e544b9bc8853709a76f08c51
ballsona0925@naver.com <ballsona0925@naver.com>
2023-08-06 00:13:48 +0900

    feat: update Marker image url

--- src/utils/useKakaoMap.tsx
+++ src/utils/useKakaoMap.tsx
@@ -47,7 +47,7 @@ const useKakaoMap = () => {
   // 2. 현재 유저 위치에 표시할 마커 생성하기
   const createUserMarker = useCallback(() => {
     const markerImage = new kakao.maps.MarkerImage(
-      '/userMarkerIcon.svg',
+      'https://jungminbuckets.s3.ap-northeast-2.amazonaws.com/Marker.svg',
       new kakao.maps.Size(54, 64),
       { offset: new kakao.maps.Point(27, 69) },
     );

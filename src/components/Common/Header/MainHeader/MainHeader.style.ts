9108d432f5def871762d47663a4f5dbe88787f45
JjungminLee <julie0964@naver.com>
2023-08-06 17:46:02 +0900

    fix:로고 에러

--- src/components/Common/Header/MainHeader/MainHeader.style.ts
+++ src/components/Common/Header/MainHeader/MainHeader.style.ts
@@ -17,9 +17,9 @@ export const Header = styled.div`
   align-items: center;
 `;
 export const Logo = styled.div`
-  width: 70px;
-  height: 50px;
-  background-image: url("https://jungminbuckets.s3.ap-northeast-2.amazonaws.com/logo.png");
+  width: 100px;
+  height: 70px;
+  background-image: url("https://jungminbuckets.s3.ap-northeast-2.amazonaws.com/Logo_final.svg");
   background-repeat: no-repeat;
   background-size: cover;
 `;
